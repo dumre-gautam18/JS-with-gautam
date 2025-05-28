aee1d0b3ad648e01d24a23df49c8121d6f382770
dumre-gautam18 <gautamdumre1718@gmail.com>
2025-05-28 03:57:17 +0000

    datatype

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,19 @@
+let myName = "gautam"
+let age = 18
+let isLoggedIn = false
+let state;
+let temperature = null;
+
+console.log(myName)
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol
+
+// object
+
+console.log(typeof age)
