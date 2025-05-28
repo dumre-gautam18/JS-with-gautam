44aa7b6ec4af1e22b6a4fd5f2f38daaf53b1ee32
dumre-gautam18 <gautamdumre1718@gmail.com>
2025-05-28 04:52:10 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,11 @@
+let someNumber = 33
+
+let changeNumber = String(someNumber)
+console.log(changeNumber);
+console.log(typeof changeNumber)
+
+
+let result = "pass";
+let myResult = Boolean(result);
+
+console.log(myResult)
