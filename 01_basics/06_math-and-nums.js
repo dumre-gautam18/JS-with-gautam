4d29d87c7222b5012106ea3f7bd81fedb2018b86
dumre-gautam18 <gautamdumre1718@gmail.com>
2025-06-06 09:53:06 +0000

    math and nums practice

---
+++ 01_basics/06_math-and-nums.js
@@ -0,0 +1,47 @@
+/* 
+const score = 500;
+console.log(score);
+
+const balance = new Number(100000);
+console.log(balance);
+
+console.log(balance.toFixed(2));
+
+
+const otherNumber  = 123.89
+console.log(otherNumber.toPrecision(3));
+// it is used to get precise value of something calculation.
+
+const hundreds = 100000;
+console.log(hundreds.toLocaleString('en-NR'));
+
+*/
+
+// ---------- Maths---------------------
+/*
+console.log(Math);
+console.log(Math.abs(-4));
+
+console.log(Math.round(4.6));
+
+console.log(Math.random());
+
+*/
+
+// const max = 20;
+// const min = 10;
+ 
+
+// console.log(Math.floor(Math.random() * (max - min) + 1) + min);
+
+const maxValue = 10;
+const minValue = 5;
+
+console.log(Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue );
+
+
+
+
+
+
+
