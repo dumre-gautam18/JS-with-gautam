b53d65e9781ceab4740f63fe36e317a5251ccdd1
dumre-gautam18 <gautamdumre1718@gmail.com>
2025-06-09 03:46:08 +0000

    array practice

---
+++ 02_basics/01_array.js
@@ -0,0 +1,42 @@
+// Array 
+
+const myArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// console.log(myAarr);
+// console.log(myArr[0]);
+
+// const myHeroes = ["shaktiman", "marvel", "superman", "spiderman"];
+// console.log(myHeroes[3]);
+
+// const myArr2= new Array("gautam", 22, "student");
+// console.log(myArr2[2]);
+
+// array methods
+ 
+// console.log(myArr.push(10));
+
+// console.log(myArr.pop());
+
+// console.log(myArr.unshift(3));
+
+// console.log(myArr.shift());
+
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+
+const newArr = myArr.join()
+
+console.log(myArr);
+console.log(newArr);
+console.log(typeof(newArr));
+console.log(newArr);
+
+
+
+
+
+
+
+
+
