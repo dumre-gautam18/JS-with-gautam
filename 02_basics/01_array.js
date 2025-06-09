2f1b19d8123e6e36ed5286bf2317d658594d068e
dumre-gautam18 <gautamdumre1718@gmail.com>
2025-06-09 06:24:50 +0000

    array basic complete

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -24,13 +24,30 @@ const myArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 // console.log(myArr.includes(9));
 // console.log(myArr.indexOf(3));
 
-
+// join is used to get values in string.
 const newArr = myArr.join()
 
+// console.log(myArr);
+// console.log(newArr);
+// console.log(typeof(newArr));
+ 
+// slicec and splice method
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1, 3);
+console.log(myn1);
+
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1, 3);
+console.log(myn2);
 console.log(myArr);
-console.log(newArr);
-console.log(typeof(newArr));
-console.log(newArr);
+
+
+
+
+
 
 
 
