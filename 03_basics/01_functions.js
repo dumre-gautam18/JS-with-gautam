f4fc87152e7e1fbd7278afe0963efee12b406226
dumre-gautam18 <gautamdumre1718@gmail.com>
2025-06-10 04:20:21 +0000

    function practice in js

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,11 @@
+// functions in javascript
+
+function addTwoNumber(num1, num2){
+   // console.log(num1+num2);
+    //let result = num1 + num2;
+    return num1 + num2;
+
+}
+
+const result = addTwoNumber(4,5);
+console.log("Result: ", result);
