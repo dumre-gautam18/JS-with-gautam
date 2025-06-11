b88b069f81ae28bff9a79335f30f2f397fdd9c9c
dumre-gautam18 <gautamdumre1718@gmail.com>
2025-06-11 09:15:06 +0000

    iteration practice

---
+++ 05_iterations/one.js
@@ -0,0 +1,24 @@
+// for (let index = 1; index < 10; index++) {
+//     const element = index;
+//     console.log(element);
+    
+// }
+
+// multiplication table of 1 to 10.
+
+for(let i = 1; i <= 10; i++){
+    //console.log(`Multiplication of ${i}`);
+    
+    for (let j = 1; j <= 10; j++){
+     // console.log(`${i} * ${j} =  ${i*j}`);
+     
+    }
+}
+
+const myHeros = ["thor", "flash", "spiderman", "shaktiman"];
+
+for (let index = 0; index < myHeros.length; index++) {
+    const element = myHeros[index];
+    console.log(element);
+    
+}
