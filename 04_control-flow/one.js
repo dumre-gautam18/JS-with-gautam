b244a41d178e21ccbe06ca23b3653bbe5c193e71
dumre-gautam18 <gautamdumre1718@gmail.com>
2025-06-11 06:59:49 +0000

    simple if esle practice

---
+++ 04_control-flow/one.js
@@ -0,0 +1,25 @@
+if(2==2){
+    console.log("user is logged in successfully");
+}
+
+const isLoggedIn = true;
+const emailLoggedIn = true;
+const googleLoggedIn = true;
+
+if(isLoggedIn && emailLoggedIn && googleLoggedIn){
+    console.log("user login from various account");
+}
+
+
+
+if(isLoggedIn || emailLoggedIn || googleLoggedIn){
+    console.log("User is verified & login successfulty");
+}
+
+
+const price = 800;
+
+if(price<1000){
+    console.log("price is less  than 1000 ");
+}
+
---
+++ 04_control-flow/switch.js
@@ -0,0 +1,24 @@
+// syantax of switch
+// switch (key){
+//     case 1:
+//         console.log("January");
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "January";
+
+switch(month){
+    case "january":
+        console.log("Month is January");
+        break;
+    case "february":
+        console.log("Month is February");
+        break;
+    
+    default:
+        break;
+        
+}
