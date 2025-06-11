31c0990ba20d08d7035a635c9a6730b3a00dae74
dumre-gautam18 <gautamdumre1718@gmail.com>
2025-06-11 10:35:44 +0000

    while and do while practice

---
+++ 05_iterations/two.js
@@ -0,0 +1,13 @@
+let value = 5;
+while(value <= 10){
+    console.log(value)
+    value ++;
+}
+
+let myArray = ["flash", "thor", "spiderman", "batman"];
+let arr = 0;
+
+while(arr < myArray.length){
+    console.log(myArray[arr]);
+    arr++;
+}
