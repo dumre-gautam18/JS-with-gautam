3bf217d6e84d06a15d66b7ad25951961f23f905c
dumre-gautam18 <gautamdumre1718@gmail.com>
2025-06-12 15:43:42 +0000

    simple iterations practice

--- 05_iterations/two.js
+++ 05_iterations/two.js
@@ -1,13 +1,21 @@
-let value = 5;
-while(value <= 10){
-    console.log(value)
-    value ++;
-}
+// let value = 5;
+// while(value <= 10){
+//     console.log(value)
+//     value ++;
+// }
 
 let myArray = ["flash", "thor", "spiderman", "batman"];
 let arr = 0;
 
 while(arr < myArray.length){
-    console.log(myArray[arr]);
+    console.log(`Value is ${myArray[arr]}`);
     arr++;
 }
+
+// do while loop practice
+let score = 11;
+
+do {
+    console.log(`score is ${score}`);
+    score ++;
+} while (score < 10);
